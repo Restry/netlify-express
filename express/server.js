c924501608ce96d47e263c8dd1512cbafe3498d6
restry <michael.wei@foxmail.com>
2019-11-08 17:12:44 +0800

    Update server.js

--- express/server.js
+++ express/server.js
@@ -17,7 +17,8 @@ router.post('/', (req, res) => res.json({ postBody: req.body }));
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
-app.use('/test', (req, res) => {
+
+app.get('/test', (req, res) => {
   res.send('test'+new Date());
   res.end();
   console.log('test'+new Date())
